6b287e69778f1411ecc237f1ab4b35894f21060e
shaqsnake <shaqsnake@gmail.com>
2024-05-04 07:50:56 +0800

    feat: add http static file serve.

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ async fn main() -> Result<()> {
         },
         SubCommand::Http(http_opts) => match http_opts {
             HttpSubCommand::Serve(opts) => {
-                process_http_serve(&opts.dir, opts.port).await?;
+                process_http_serve(opts.dir, opts.port).await?;
             }
         },
     }
--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -1,19 +1,56 @@
 use anyhow::Result;
-use axum::{routing::get, Router};
-use std::{net::SocketAddr, path::Path};
-use tracing::info;
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    routing::get,
+    Router,
+};
+use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tracing::{info, warn};
 
-pub async fn process_http_serve(path: &Path, port: u16) -> Result<()> {
+#[derive(Debug)]
+struct HttpServeState {
+    path: PathBuf,
+}
+
+pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving directory {:?} on port {}", path, port);
 
-    let router = Router::new().route("/", get(index_handler));
+    let router = Router::new()
+        .route("/*path", get(file_handler))
+        .with_state(Arc::new(HttpServeState { path }));
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
 
     Ok(())
 }
 
-async fn index_handler() -> &'static str {
-    "Hello, World!"
+async fn file_handler(
+    State(state): State<Arc<HttpServeState>>,
+    Path(path): Path<String>,
+) -> (StatusCode, String) {
+    let p = std::path::Path::new(&state.path).join(path);
+    info!("Reading file {:?}", p);
+
+    if !p.exists() {
+        (
+            StatusCode::NOT_FOUND,
+            format!("File {} not Found", p.display()),
+        )
+    } else {
+        match tokio::fs::read_to_string(p).await {
+            Ok(content) => {
+                info!("Read {} bytes", content.len());
+                (StatusCode::OK, content)
+            }
+            Err(e) => {
+                warn!("Error reading file: {:?}", e);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Error reading file: {}", e),
+                )
+            }
+        }
+    }
 }
