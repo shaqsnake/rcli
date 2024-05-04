983a83070e6dc3460c4f9718d6bbb075cf5d76f7
shaqsnake <shaqsnake@gmail.com>
2024-05-04 09:08:14 +0800

    feat: support static file serve with tower-http, and add some unittests.

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -6,6 +6,7 @@ use axum::{
     Router,
 };
 use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use tower_http::services::ServeDir;
 use tracing::{info, warn};
 
 #[derive(Debug)]
@@ -17,9 +18,11 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     info!("Serving directory {:?} on port {}", path, port);
 
+    let state = Arc::new(HttpServeState { path: path.clone() });
     let router = Router::new()
+        .nest_service("/tower", ServeDir::new(path))
         .route("/*path", get(file_handler))
-        .with_state(Arc::new(HttpServeState { path }));
+        .with_state(state);
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
 
@@ -39,6 +42,10 @@ async fn file_handler(
             format!("File {} not Found", p.display()),
         )
     } else {
+        // TODO: test p is a directory
+        // if it is a directory, list all files/subdirectories
+        // as <li><a href="/path/to/file">file name</a></li>
+        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
@@ -52,5 +59,41 @@ async fn file_handler(
                 )
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_file_handler() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        assert_eq!(status, StatusCode::OK);
+        assert!(content.trim().starts_with("[package]"));
+    }
+
+    #[tokio::test]
+    async fn test_file_handler_not_found() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) = file_handler(State(state), Path("not-exists".to_string())).await;
+        assert_eq!(status, StatusCode::NOT_FOUND);
+        assert!(content.contains("not Found"));
+    }
+
+    #[tokio::test]
+    async fn test_file_handler_error() {
+        let state = Arc::new(HttpServeState {
+            path: PathBuf::from("."),
+        });
+        let (status, content) =
+            file_handler(State(state), Path("fixtures/ed25519.pk".to_string())).await;
+        assert_eq!(status, StatusCode::INTERNAL_SERVER_ERROR);
+        assert!(content.contains("Error"));
     }
 }
