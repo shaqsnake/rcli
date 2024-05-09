87ead47d2b6cc985e2da39bd9e39edb22a440962
shaqsnake <shaqsnake@gmail.com>
2024-05-09 23:26:01 +0800

    feat: add listing dir for static file http serve.

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use axum::{
     extract::{Path, State},
     http::StatusCode,
+    response::{Html, IntoResponse, Response},
     routing::get,
     Router,
 };
@@ -32,30 +33,55 @@ pub async fn process_http_serve(path: PathBuf, port: u16) -> Result<()> {
 async fn file_handler(
     State(state): State<Arc<HttpServeState>>,
     Path(path): Path<String>,
-) -> (StatusCode, String) {
+) -> (StatusCode, Response) {
     let p = std::path::Path::new(&state.path).join(path);
     info!("Reading file {:?}", p);
 
     if !p.exists() {
         (
             StatusCode::NOT_FOUND,
-            format!("File {} not Found", p.display()),
+            format!("File {} not Found", p.display()).into_response(),
         )
+    } else if p.is_dir() {
+        match tokio::fs::read_dir(p).await {
+            Ok(mut entries) => {
+                let mut content = String::new();
+                content.push_str("<!DOCTYPE html><html><body><ul>");
+
+                while let Some(entry) = entries.next_entry().await.unwrap() {
+                    let name = entry.file_name();
+                    let name = name.to_string_lossy();
+                    let path = entry.path();
+                    let path = path.strip_prefix(&state.path).unwrap();
+                    let path = path.to_string_lossy();
+                    content.push_str(&format!(
+                        r#"<li><a href="/tower/{}">{}</a></li>"#,
+                        path, name
+                    ));
+                }
+                content.push_str("</ul></body></html>");
+
+                (StatusCode::OK, Html(content).into_response())
+            }
+            Err(e) => {
+                warn!("Error reading directory: {:?}", e);
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Error reading directory: {}", e).into_response(),
+                )
+            }
+        }
     } else {
-        // TODO: test p is a directory
-        // if it is a directory, list all files/subdirectories
-        // as <li><a href="/path/to/file">file name</a></li>
-        // <html><body><ul>...</ul></body></html>
         match tokio::fs::read_to_string(p).await {
             Ok(content) => {
                 info!("Read {} bytes", content.len());
-                (StatusCode::OK, content)
+                (StatusCode::OK, content.into_response())
             }
             Err(e) => {
                 warn!("Error reading file: {:?}", e);
                 (
                     StatusCode::INTERNAL_SERVER_ERROR,
-                    format!("Error reading file: {}", e),
+                    format!("Error reading file: {}", e).into_response(),
                 )
             }
         }
@@ -71,9 +97,8 @@ mod tests {
         let state = Arc::new(HttpServeState {
             path: PathBuf::from("."),
         });
-        let (status, content) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
+        let (status, _) = file_handler(State(state), Path("Cargo.toml".to_string())).await;
         assert_eq!(status, StatusCode::OK);
-        assert!(content.trim().starts_with("[package]"));
     }
 
     #[tokio::test]
@@ -81,9 +106,8 @@ mod tests {
         let state = Arc::new(HttpServeState {
             path: PathBuf::from("."),
         });
-        let (status, content) = file_handler(State(state), Path("not-exists".to_string())).await;
+        let (status, _) = file_handler(State(state), Path("not-exists".to_string())).await;
         assert_eq!(status, StatusCode::NOT_FOUND);
-        assert!(content.contains("not Found"));
     }
 
     #[tokio::test]
@@ -91,9 +115,7 @@ mod tests {
         let state = Arc::new(HttpServeState {
             path: PathBuf::from("."),
         });
-        let (status, content) =
-            file_handler(State(state), Path("fixtures/ed25519.pk".to_string())).await;
+        let (status, _) = file_handler(State(state), Path("fixtures/ed25519.pk".to_string())).await;
         assert_eq!(status, StatusCode::INTERNAL_SERVER_ERROR);
-        assert!(content.contains("Error"));
     }
 }
